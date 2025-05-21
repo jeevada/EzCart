8459ea28be68f9522365f3818452063d4609a172
jeevada <jeevathepath@gmail.com>
2025-05-21 22:22:26 +0530

    feature: product model and create product end point

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())  // accept the json data request otherwise it will refuse to accept the json data in request
 const products = require('./routes/product')
 
 app.use('/api/v1', products)
