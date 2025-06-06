35378467dc5da1cab8a397eb5ed1173491b905e9
jeevada <jeevathepath@gmail.com>
2025-06-07 05:23:50 +0530

    Feature: authentication and authorization routes

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
-const errorMiddleware = require("./middlewares/error")
+const errorMiddleware = require("./middlewares/error");
+const products = require('./routes/product');
+const auth = require('./routes/auth');
+const cookieParser = require("cookie-parser");
+const qs = require('qs');
+
 
 app.use(express.json());  // accept the json data request otherwise it will refuse to accept the json data in request
-const products = require('./routes/product')
+app.use(cookieParser());
+
+// Parse nested query parameters like ?price[lt]=100 → { price: { lt: 100 } }
+// Use qs to parse nested query params like ?price[lt]=100
+app.set("query parser", function (str) {
+  return qs.parse(str);
+});
+
+
 
 app.use('/api/v1', products);
+app.use('/api/v1', auth);
+// Global error handler
 app.use(errorMiddleware);
 
 module.exports = app;
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
 const router = express.Router();
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/authenticate');
 
-router.route('/products').get(getProducts);
-router.route('/product/new').post(newProduct);
+router.route('/products').get(isAuthenticatedUser, getProducts);
+router.route('/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
 router.route('/product/:id')
     .get(getSingleProduct)
     .put(updateProduct)
     .delete(deleteProduct);
 
-module.exports = router
+module.exports = router 
