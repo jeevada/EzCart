0ebe5fc183b05cdb6b550fe897182fad30f35f5d
jeevada <jeevathepath@gmail.com>
2025-07-11 20:23:43 +0530

    feature: filter

--- frontend/src/actions/productsActions.js
+++ frontend/src/actions/productsActions.js
@@ -1,13 +1,25 @@
 import axios from "axios";
 import { productsFail, productsRequest, productsSuccess } from "../slices/productsSlice";
 
-export const getProducts = (keyword, currentPage) => async (dispatch) => {
+export const getProducts = (keyword, price, category, rating, currentPage) => async (dispatch) => {
     try {
         dispatch(productsRequest())
         let link = `/api/v1/products?page=${currentPage}`;
 
         if(keyword) {
             link += `&keyword=${keyword}`
+        }
+
+        if(price) {
+            link += `&price[gte]=${price[0]}&price[lte]=${price[1]}`
+        }
+
+        if(category) {
+            link += `&category=${category}`
+        }
+
+        if(rating) {
+            link += `&ratings=${rating}`
         }
 
         const { data } = await axios.get(link);  
